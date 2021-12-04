6eb2763ca9801539ae1350ca5ea7163d76bb0319
Adam Volkman <itismaku@gmail.com>
2021-12-04 19:57:03 +0100

    project

---
+++ src/main/java/cz/maku/topvoters/TopvotersApplication.java
@@ -0,0 +1,13 @@
+package cz.maku.topvoters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopvotersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopvotersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/maku/topvoters/TopvotersApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.maku.topvoters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopvotersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
